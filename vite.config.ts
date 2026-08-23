3dd6442cd4e2cff8a8093395e0e550f3b0452221
Daniel Kurtz <djkurtz@google.com>
2026-08-23 03:22:24 +0000

    Allow proxy hosts in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   base: './', // Using relative paths so it works seamlessly on GitHub Pages
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    allowedHosts: true
   }
 });
